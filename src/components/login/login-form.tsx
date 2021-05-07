86547b5ea749d841e176fc2cae840d7b3e716850
Louis Andersen <lhl@multi-it.dk>
2021-05-07 07:02:02 -0700

    Remove register and forgot password <Admin access no need these features>

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -2,9 +2,8 @@ import React, { useState, useRef, useCallback } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Form, { Item, Label, ButtonItem, ButtonOptions, RequiredRule, EmailRule } from 'devextreme-react/form';
 import LoadIndicator from 'devextreme-react/load-indicator';
-import notify from 'devextreme/ui/notify';
-
 import './login-form.scss';
+import { Grid } from '@material-ui/core';
 
 const LoginForm = () => {
   const history = useHistory();
@@ -18,40 +17,30 @@ const LoginForm = () => {
     history.push('/create-account');
   }, [history]);
 
+  // @ts-ignore
   return (
-    <form className={'login-form'} onSubmit={onSubmit}>
-      <Form formData={formData.current} disabled={loading}>
-        <Item dataField={'email'} editorType={'dxTextBox'} editorOptions={emailEditorOptions}>
-          <RequiredRule message="Email is required" />
-          <EmailRule message="Email is invalid" />
-          <Label visible={false} />
-        </Item>
-        <Item dataField={'password'} editorType={'dxTextBox'} editorOptions={passwordEditorOptions}>
-          <RequiredRule message="Password is required" />
-          <Label visible={false} />
-        </Item>
-        <Item dataField={'rememberMe'} editorType={'dxCheckBox'} editorOptions={rememberMeEditorOptions}>
-          <Label visible={false} />
-        </Item>
-        <ButtonItem>
-          <ButtonOptions width={'100%'} type={'default'} useSubmitBehavior={true}>
+        <form className={'login-form'} onSubmit={onSubmit}>
+              <Form formData={formData.current} disabled={loading}>
+                  <Item dataField={'email'} editorType={'dxTextBox'} editorOptions={emailEditorOptions}>
+                      <RequiredRule message="Email is required" />
+                      <EmailRule message="Email is invalid" />
+                      <Label visible={false} />
+                  </Item>
+                  <Item dataField={'password'} editorType={'dxTextBox'} editorOptions={passwordEditorOptions}>
+                      <RequiredRule message="Password is required" />
+                      <Label visible={false} />
+                  </Item>
+                  <ButtonItem>
+                      <ButtonOptions width={'100%'} type={'default'} useSubmitBehavior={true}>
             <span className="dx-button-text">
               {loading ? <LoadIndicator width={'24px'} height={'24px'} visible={true} /> : 'Sign In'}
             </span>
-          </ButtonOptions>
-        </ButtonItem>
-        <Item>
-          <div className={'link'}>
-            <Link to={'/reset-password'}>Forgot password?</Link>
-          </div>
-        </Item>
-        <ButtonItem>
-          <ButtonOptions text={'Create an account'} width={'100%'} onClick={onCreateAccountClick} />
-        </ButtonItem>
-      </Form>
-    </form>
+                      </ButtonOptions>
+                  </ButtonItem>
+              </Form>
+          </form>
   );
-};
+}
 
 const emailEditorOptions = { stylingMode: 'filled', placeholder: 'Email', mode: 'email' };
 const passwordEditorOptions = { stylingMode: 'filled', placeholder: 'Password', mode: 'password' };
